8e75d2031b140fd5ac598ee160b54d434882317d
zhlyakh <83218437+zhlyakh@users.noreply.github.com>
2021-04-27 00:48:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// let a = 34;
+// console.log(typeof a);
+// console.log(a);
+// const a = 54;
+// a = 65;
+// console.log(a);
+// var name = 'David';
+// alert(`Hello ${name}`);
+
+let name = prompt('как тебя зовут?');
+alert('your name is ' + name);
